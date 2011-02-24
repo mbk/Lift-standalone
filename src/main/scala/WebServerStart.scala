45cd140c0ed5fa33b48d3231af4c01934a205176
Maarten Koopmans <maarten.koopmans@gmail.com>
2011-02-24 18:14:45 +0100

    Intermediate commit before considering something else.

--- src/main/scala/WebServerStart.scala
+++ src/main/scala/WebServerStart.scala
@@ -1,21 +1,50 @@
-import org.eclipse.jetty.webapp.{WebAppContext}
+import org.eclipse.jetty.webapp.{WebAppContext,WebAppClassLoader}
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.nio._
+import org.eclipse.jetty.util.{URIUtil}
+import java.io.{File}
+import java.net.{URL,URLClassLoader}
+import java.security.ProtectionDomain
 
 object WebServerStart   {
 	
-		def main(args:Array[String]) {
-			val server = new Server
+		def main(args:Array[String]) {			
+			
+			val protectionDomain = WebServerStart.getClass().getProtectionDomain()
+			val location = protectionDomain.getCodeSource().getLocation()
+			Console println("**** location = " + location.toExternalForm)
+			val locations = Array[URL](location)			
+			Thread.currentThread().setContextClassLoader(new URLClassLoader(locations))
+
+
+			val context = new WebAppContext();
+			val webAppClassLoader = new WebAppClassLoader(context)
+			//Thread.currentThread().setContextClassLoader(webAppClassLoader);
+			
+			context.setClassLoader(webAppClassLoader)
+			context.setContextPath(URIUtil.SLASH)
+			
+			val tempDir = System.getProperty("jettyTempDir")
+			if( tempDir != null ) {
+	            val tempDirectory = new File(tempDir);
+	            context.setTempDirectory(tempDirectory);
+	        }
+			
+
+			context.setWar(location.toExternalForm)
+			
 			val scc = new SelectChannelConnector
 			scc.setPort(8080)
-			server.setConnectors(Array(scc))
-		
-			val context = new WebAppContext();
-			context.setContextPath("/*")
-			context.setWar("src/main/webapp")
+			val server = new Server
+			server.setConnectors(Array(scc));
 			server.setHandler(context)
-			Console println("Jetty started")
-		    server.start()
-		    server.join()
+
+			try {
+				Console println("Starting Jetty")
+			    server.start()
+			    server.join()
+			} catch {
+				case e: Exception => { e printStackTrace}
+			}
 		}
 }
